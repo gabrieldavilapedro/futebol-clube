8da7e57604e80ef7c972fb0a4f9ad12a64a2dc59
Gabriel D'avila Pedro <gabrieldavilapedroo@gmail.com>
2024-03-07 14:41:15 -0300

    feat: req 13, criando migration e model para Mathc

---
+++ app/backend/src/database/models/Match.model.ts
@@ -0,0 +1,70 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
+import TeamModel from './Team.Model';
+
+class MatchModel extends Model<InferAttributes<MatchModel>, InferCreationAttributes<MatchModel>> {
+  declare id: CreationOptional<number>;
+  declare homeTeamId: number;
+  declare homeTeamGoals: number;
+  declare awayTeamId: number;
+  declare awayTeamGoals: number;
+  declare inProgress: boolean;
+  declare homeTeam?: TeamModel;
+  declare awayTeam?: TeamModel;
+}
+
+MatchModel.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  homeTeamId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    field: 'home_team_id',
+  },
+  homeTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    field: 'home_team_goals',
+  },
+  awayTeamId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    field: 'away_team_id',
+  },
+  awayTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    field: 'away_team_goals',
+  },
+  inProgress: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: false,
+    field: 'in_progress',
+  },
+}, {
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+});
+
+MatchModel.hasOne(TeamModel, {
+  foreignKey: 'id',
+  as: 'homeTeamId',
+});
+MatchModel.hasOne(TeamModel, {
+  foreignKey: 'id',
+  as: 'awayTeamId',
+});
+
+export default MatchModel;
